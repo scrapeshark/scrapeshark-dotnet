d26fd9bbe8ba905be238ba70f8b8e1718f638b59
aevitas <aevitas@users.noreply.github.com>
2022-07-18 22:40:00 +0200

    Add save screenshot helper

---
+++ src/ScrapeShark/Extensions/ScreenshotResultExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace ScrapeShark.Extensions;
+
+public static class ScreenshotResultExtensions
+{
+    public static async Task SaveToFileAsync(this ScreenshotResult result, string fileName)
+    {
+        if (string.IsNullOrWhiteSpace(fileName))
+            throw new ArgumentNullException(nameof(fileName));
+
+        var fi = new FileInfo(fileName);
+
+        if (fi.Exists)
+            throw new ArgumentException($"A file with {fileName} already exists");
+
+        await using var fs =
+            new FileStream(File.OpenHandle(fileName, FileMode.CreateNew, FileAccess.Write), FileAccess.Write);
+
+        await fs.WriteAsync(result.Buffer, 0, result.Buffer.Length);
+    }
+}
